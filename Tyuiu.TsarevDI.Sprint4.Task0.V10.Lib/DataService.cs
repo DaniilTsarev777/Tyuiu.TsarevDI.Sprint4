6e1711d9012e0c171b73d7a67e1a9166e3f5c8f6
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-31 16:32:06 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint4.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.TsarevDI.Sprint4.Task0.V10.Lib
+{
+    public class DataService : ISprint4Task0V10
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int[] m = { 9, 8, 7, 9, 5, 4, 3, 2, 3, 7 };
+            int s = 0;
+            for (int i = 0; i < 10; i++)
+                if (m[i] % 2 != 0)
+                    s += m[i];
+            return s;
+        }
+    }
+}
