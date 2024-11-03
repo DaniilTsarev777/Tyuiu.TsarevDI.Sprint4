f27853ad1e0b95e1656ff3a678e3095cc6255561
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-03 17:56:01 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint4.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.TsarevDI.Sprint4.Task2.V6.Lib;
+namespace Tyuiu.TsarevDI.Sprint4.Task2V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] m = { 4, 4, 4, 4, 4, 4, 5, 7, 5, 3, 4, 4, 6, 8, 8 };
+            Assert.AreEqual(525, ds.Calculate(m));
+
+
+
+
+
+        }
+    }
+}
