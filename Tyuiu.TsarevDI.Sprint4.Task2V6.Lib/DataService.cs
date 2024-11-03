da1b0ee9720f4799a7a714e33bcf2580927312b6
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-03 16:05:21 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint4.Task2V6.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.TsarevDI.Sprint4.Task2V6.Lib
+{
+    public class DataService : ISprint4Task2V6
+    {
+        public int Calculate(int[] m)
+        {
+            int r;
+            r = 1;
+            for (int i = 0; i < m.Length; i++)
+                if (m[i] % 2 != 0)
+                    r *= m[i];
+            return r;
+
+        }
+    }
+}
