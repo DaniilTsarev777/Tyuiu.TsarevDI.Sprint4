30fb7cd3dfb3fa371836e4f232072dbcbdf0efd7
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-03 16:39:58 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint4.Task2.V6/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.TsarevDI.Sprint4.Task2.V6.Lib;
+namespace Tyuiu.TsarevDI.Sprint4.Task2V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #4                                                             *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                 *");
+            Console.WriteLine("* Задание #2                                                            *");
+            Console.WriteLine("* Варинат #6                                                            *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine("Введите количество элементов массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int[] m = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                m[i] = rnd.Next(2, 8);
+            }
+
+            Console.WriteLine("массив");
+            for (int i = 0; i < len; i++)
+                Console.WriteLine(m[i] + "\t");
+
+
+            
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.Calculate(m));
+            Console.ReadKey();
+        }
+    }
+}
