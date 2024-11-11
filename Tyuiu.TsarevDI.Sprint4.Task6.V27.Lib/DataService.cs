7174c13166b8fc80949bd51864515ed70796f85a
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-11 22:08:59 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint4.Task6.V27.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.TsarevDI.Sprint4.Task6.V27.Lib
+{
+    public class DataService : ISprint4Task6V27
+    {
+        public int Calculate(string[] array)
+        {
+            int s;
+            s = 0;
+            string[] m = new string[] { "Квадрат", "Прямоугольник", "Круг", "Треугольник", "Пятиугольник", "Шестиугольник", "Восьмиугольник" };
+            foreach (string element in m)
+                s = m.Count(t => t.Length < 7);
+                
+                    
+            return s;
+
+        }
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint4.Task6.V27/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.TsarevDI.Sprint4.Task6.V27.Lib;
+namespace Tyuiu.TsarevDI.Sprint4.Task6.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #4                                                             *");
+            Console.WriteLine("* Тема: Класс Array                                                     *");
+            Console.WriteLine("* Задание #6                                                            *");
+            Console.WriteLine("* Варинат #27                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+
+            string[] m = new string[] { "Квадрат", "Прямоугольник", "Круг", "Треугольник", "Пятиугольник", "Шестиугольник", "Восьмиугольник" };
+
+            Console.WriteLine("Исходный массив:");
+            foreach (string element in m)
+                Console.WriteLine(element);
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Количество элементов с длинной меньше 7:");
+
+            Console.WriteLine(ds.Calculate(m));
+            Console.ReadKey();
+
+
+        }
+    }
+}
