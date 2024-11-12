286b0cf4e69b3aab6f5711e0b539ba970dab7f12
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-12 18:44:25 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint4.Task7.V28.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.TsarevDI.Sprint4.Task7.V28.Lib
+{
+    public class DataService : ISprint4Task7V28
+    {
+        public int Calculate(int rows, int columns, string value)
+        {
+            int[,] m = new int[rows, columns];
+
+            int p = 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    m[i, j] = int.Parse(value.Substring(i*columns+j, 1));
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    if (m[i, j] % 2 == 0)
+                        p *= m[i, j];
+
+            return p;
+
+
+        }
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint4.Task7.V28/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.TsarevDI.Sprint4.Task7.V28.Lib;
+namespace Tyuiu.TsarevDI.Sprint4.Task7.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #4                                                             *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту               *");
+            Console.WriteLine("* Задание #7                                                            *");
+            Console.WriteLine("* Варинат #28                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+
+            int rows, columns, index;
+            rows = 5;
+            columns = 3;
+
+            string str = "623351179845632";
+
+            int[,] m = new int[rows, columns];
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            index = 0;
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+
+
+            Console.WriteLine("произведение четных чисел = " + ds.Calculate(rows, columns, str));
+            Console.ReadKey();
+
+        }
+    }
+}
